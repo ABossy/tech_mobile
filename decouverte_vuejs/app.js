5b9e53d7592434eeaf863349f2ee3ae74ea5e134
Astrid Bossy <astrid.bossy@Digital-Grenoble.local>
2018-03-01 16:28:11 +0100

    techno mobile decouverte j1

---
+++ decouverte_vuejs/app.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#app',
+    // el = element ; #app = Div du fichier html
+    data: {
+        name :'',
+        firstname:'',
+        active: false,
+        show :true,
+        message : 'machine Cafe',
+        link : 'http://astrid.mcbandanas.online' //on retrouve nos variable du fichier html.
+    }
+
+})
+
+
+       
+       
+        
+
