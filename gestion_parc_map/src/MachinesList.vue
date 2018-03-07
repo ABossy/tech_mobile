9473fd3d0058f60d150f8d3546d8f8267260d981
Astrid Bossy <astrid.bossy@Digital-Grenoble.local>
2018-03-07 11:31:06 +0100

    Ajout des marqueurs

--- gestion_parc_map/src/MachinesList.vue
+++ gestion_parc_map/src/MachinesList.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
     <h1>Liste des Machines</h1>
+    
    <machines></machines>
 </div>
 </template>
--- gestion_parc_map/src/MachinesMap.vue
+++ gestion_parc_map/src/MachinesMap.vue
@@ -1,13 +1,23 @@
 <template>
  <div>
 <h1>Map des Machines</h1>
+
    <gmap-map
   :center="{lat:10, lng:10}"
   :zoom="7"
-  style="width: 100%; height: 800px"
->
+  style="width: 100%; height: 800px">
+<gmap-marker
+      :key="machine.id"
+      v-for="machine in machines"
+      :position="{lat:machine.latitude,
+                  lng:machine.longitude}"
+      :clickable="true"
+      :draggable="true"
+      >
+      </gmap-marker>
 </gmap-map>
 </div>
+
 </template>
 
 <script>
@@ -15,7 +25,22 @@ export default {
   name: "machine-map",
 
   data() {
-    return {};
+    return {
+      machines: [
+        {
+          id: 1,
+          latitude: 10,
+          longitude: 10,
+        },
+        {
+          id: 2,
+          latitude: 11,
+          longitude: 9.6,
+        }
+]
+   
+    };
   }
 };
 </script>
+
